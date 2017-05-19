ed6b4f3b2f9dc24f9ee5d2b40dc12ae5bb1028b2
vseqa1 <vseqa1@KETTY0417>
2017-05-19 02:49:23 -0700

    Single byte !"#)6=@Aa}~
    Double byte
    啊齄丂狛狜隣郎隣兀﨩ˊ▇█〞〡￤℡㈱‐ー﹡﹢﹫、〓ⅰⅹ⒈€㈠㈩ⅠⅫ！￣ぁんァヶΑ︴АЯаяāɡㄅㄩ─╋︵﹄︻︱︳︴ⅰⅹɑɡ〇〾⿻⺁䜣€
    
    Four byte
    Four byte: Ext-A           㐀㒣㕴㕵㙉㙊䵯䵰䶴䶵
    Four byte: Ext-B            𠀀𠀁𠀂𠀃𪛑𪛒𪛓𪛔𪛕𪛖
    Four byte: Mongolian   ᠀᠐᠙ᠠᡷᢀᡨᡩᡪᡫ
    Four byte: Tibetan         ༀཇཉཪཱྋ྾࿌࿏ྼྙ
    Four byte: Yi                   ꀀꒌꂋꂌꂍꂎꂔꂕ꒐꓆
    Four byte: Uighur         ،؟ئبتجدرشعەﭖﭙﯓﯿﺉﺒﻺﻼ
    Four byte: Tai Le           ᥐᥥᥦᥧᥨᥭᥰᥱᥲᥴ
    Four byte: Hangul        ᄓᄕᇬᇌᇜᇱㄱㅣ가힝
    
    ،؟ئبتجدرشعەﭖﭙﯓﯿﺉﺒﻺﻼ
    
    \\cpvsbuild\DROPS\dev11\RTMRel\raw\50723.01\binaries.x86ret\SuiteBin\i386\tfs\tests
    
    \\cpvsbuild\DROPS\dev11\RTMRel\raw\50718.00\rus\binaries.x86ret\Localized\rus\vset\common\Methdologies\Common\Resources
    
    FRA:
    éçàèùûôâêîÖÄÏÜË
    
    DEU:
    Äöüßdráóíéú§
    
    ITA:
    çé§éòàùì  àéèíìóòúùÀÉÈÍÌÓÒÚÙ
    
    KOR:
    풀로프라이 템플릿은스크럼 방법과 스크럼 용어 팀을 따르십시오.
    
    JPN:
    理事官参照してください
    亹俅俋俏俒㑨倢倞偂㑪侅伷㒒僡僙僔𠌫傱傺僄傒傣偰偪
    黧黧鼺𪘂
    鷗外
    
    ESN:
    ÑOPòÀùÈìmlñó
    
    RUS:
    АБВГДгвбаНОПРСТУФХЦЧШЩйкКИлмьэЎЇІ́ЎЇЇІїўфФт

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,55 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            /*
+             Single byte
+!"#)6=@Aa}~
+
+Double byte
+啊齄丂狛狜隣郎隣兀﨩ˊ▇█〞〡￤℡㈱‐ー﹡﹢﹫、〓ⅰⅹ⒈€㈠㈩ⅠⅫ！￣ぁんァヶΑ︴АЯаяāɡㄅㄩ─╋︵﹄︻︱︳︴ⅰⅹɑɡ〇〾⿻⺁䜣€
+
+Four byte
+Four byte: Ext-A           㐀㒣㕴㕵㙉㙊䵯䵰䶴䶵
+Four byte: Ext-B            𠀀𠀁𠀂𠀃𪛑𪛒𪛓𪛔𪛕𪛖
+Four byte: Mongolian   ᠀᠐᠙ᠠᡷᢀᡨᡩᡪᡫ
+Four byte: Tibetan         ༀཇཉཪཱྋ྾࿌࿏ྼྙ
+Four byte: Yi                   ꀀꒌꂋꂌꂍꂎꂔꂕ꒐꓆
+Four byte: Uighur         ،؟ئبتجدرشعەﭖﭙﯓﯿﺉﺒﻺﻼ
+Four byte: Tai Le           ᥐᥥᥦᥧᥨᥭᥰᥱᥲᥴ
+Four byte: Hangul        ᄓᄕᇬᇌᇜᇱㄱㅣ가힝
+
+،؟ئبتجدرشعەﭖﭙﯓﯿﺉﺒﻺﻼ
+
+\\cpvsbuild\DROPS\dev11\RTMRel\raw\50723.01\binaries.x86ret\SuiteBin\i386\tfs\tests
+
+\\cpvsbuild\DROPS\dev11\RTMRel\raw\50718.00\rus\binaries.x86ret\Localized\rus\vset\common\Methdologies\Common\Resources
+
+FRA:
+éçàèùûôâêîÖÄÏÜË
+
+DEU:
+Äöüßdráóíéú§
+
+ITA:
+çé§éòàùì  àéèíìóòúùÀÉÈÍÌÓÒÚÙ
+
+KOR:
+풀로프라이 템플릿은스크럼 방법과 스크럼 용어 팀을 따르십시오.
+
+JPN:
+理事官参照してください
+亹俅俋俏俒㑨倢倞偂㑪侅伷㒒僡僙僔𠌫傱傺僄傒傣偰偪
+黧黧鼺𪘂
+鷗外
+
+
+ESN:
+ÑOPòÀùÈìmlñó
+
+RUS:
+АБВГДгвбаНОПРСТУФХЦЧШЩйкКИлмьэЎЇІ́ЎЇЇІїўфФт
+             
+             */
         }
     }
 }
